1f4986d583d31a9322f85d2e8222f532c457e642
francisco-muniz <66228196+francisco-muniz@users.noreply.github.com>
2020-06-01 11:16:01 -0400

    Add weird solution

--- python/raindrops/raindrops.py
+++ python/raindrops/raindrops.py
@@ -1,2 +1,22 @@
 def convert(number):
-    pass
+	modulo = {
+		"3": number % 3,
+		"5": number % 5,
+		"7": number % 7
+	}
+
+	sounds = {
+		"3": "Pling",
+		"5": "Plang",
+		"7": "Plong"
+	}
+
+	noise = ""
+	for i in ("3","5","7"):
+		if modulo[i] == 0:
+			noise = noise + sounds[i]
+
+	if len(noise) > 0:
+		return f'{noise}'
+	else:	
+		return f'{number}'
